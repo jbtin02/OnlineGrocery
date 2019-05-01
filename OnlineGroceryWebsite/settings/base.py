37e9f5939b6ddf646d910a4ad73609bf7a5e371b
Bryant Tin <jbtin02@gmail.com>
2019-05-02 01:16:14 +0800

    Update Final

--- OnlineGroceryWebsite/settings/base.py
+++ OnlineGroceryWebsite/settings/base.py
@@ -85,16 +85,8 @@ WSGI_APPLICATION = 'OnlineGroceryWebsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        'ENGINE': 'django.db.backends.mysql',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # 'NAME': os.path.join(BASE_DIR, 'onlinegrocerystoredb.sql'),
-        'NAME': 'onlinegrocerystoredb',
-        'USER': 'root',
-        'PASSWORD': '',
-        'OPTIONS': {
-        'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
