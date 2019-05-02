30140349fc9a816b7f342cb4e3a35efccbdde172
Bryant Tin <jbtin02@gmail.com>
2019-05-02 14:43:45 +0800

    Update Addons

--- OnlineGroceryWebsite/settings/production.py
+++ OnlineGroceryWebsite/settings/production.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     'carts',
 ]
 
-# AUTH_USER_MODEL = 'accounts.User' #changes the built-in user model to ours
+AUTH_USER_MODEL = 'accounts.User' #changes the built-in user model to ours
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
